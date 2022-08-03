cc1c27041b096c15c0fa568ff9785f3f2665861c
Julian O <juospinam@unal.edu.co>
2022-08-02 21:42:58 -0500

    uploading files

---
+++ cypress/page/upload.page.ts
@@ -0,0 +1,31 @@
+
+class Upload {
+    private URL: string;
+    private selectFileSelector: string;
+    private uploadFileSelector: string;
+    private titleNameSelector: string;
+
+    constructor(){
+        this.URL = "https://the-internet.herokuapp.com/upload"
+        this.selectFileSelector = "#file-upload"
+        this.uploadFileSelector = "#file-submit"
+        this.titleNameSelector = "#uploaded-files"
+    }
+
+    public visitWebSite(): void{
+        cy.visit(this.URL)
+    }
+
+    public uploadFile(fileName: string): void{
+        cy.get(this.selectFileSelector).attachFile(fileName)
+        cy.get(this.uploadFileSelector).click()
+    }
+
+    public verifyTitle(data: string[]): void {
+        cy.get(this.titleNameSelector).each((item, index) => {
+            cy.wrap(item).should("contain.text", data[index])
+          })
+    }
+}
+
+export {Upload};
